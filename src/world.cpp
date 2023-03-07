731988f5177702edddd51214480c1af1ca82b72c
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-03-07 17:31:28 +0000

    World XML validation.

--- src/world.cpp
+++ src/world.cpp
@@ -18,26 +18,36 @@ World::World(WindowSize windowSize, Camera* camera, std::vector<Model> models) {
 
 World::World(XMLParser parser) {
 
-    windowSize = parser.get_node("window").as_tuple<int,int>({"width", "height"});
+    /* Getting window information. */
+    XMLParser window_node = parser.get_node("window");
+    window_node.validate_attrs({"width", "height"});
+    windowSize = window_node.as_tuple<int, int>({"width", "height"});
+    
+    /* Getting camera information. */
     camera = Camera::parse(parser.get_node({"camera"}));
+
+    /* Parsing present models to display. */
     models = std::vector<Model>();
 
     XMLParser aux = parser.get_node("group").get_node("models");
 
-    for (XMLParser& p : aux.get_nodes("model"))
+    for (XMLParser& p : aux.get_nodes("model")) {
+
+        p.validate_attrs({"file"});
         models.emplace_back(p.as_object<Shape,string>({"file"}));
+    }
 }
 
 void World::initScene() {
 
-    // put GLUT's init here
+    /* GLUT's init here. */
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(std::get<0>(windowSize), std::get<1>(windowSize));
 
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Model Viewer 3000");
 
-    // some OpenGL settings
+    /* OpenGL settings. */
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -48,20 +58,24 @@ void World::changeSize(int width, int height) {
 }
 
 void World::renderScene() {
-  // clear buffers
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  camera->setupScene();
 
-  drawAxis();
+    /* Clear buffers. */ 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    camera->setupScene();
 
-  for (Model &model : models)
-    model.draw();
+    drawAxis(); /* Drawing the x,y and z axis. */
 
-  // End of frame
-  glutSwapBuffers();
+    /* Drawing the models from the XML file. */
+    for (Model &model : models)
+        model.draw();
+
+    /* End of frame. */
+    glutSwapBuffers();
 }
 
-void World::handleKey(__attribute__((unused)) unsigned char key, __attribute__((unused)) int x, __attribute__((unused)) int y) {
+void World::handleKey(__attribute__((unused)) unsigned char key, 
+                      __attribute__((unused)) int x, 
+                      __attribute__((unused)) int y) {
     camera->handleKey(key, x, y);
 }
 
@@ -70,20 +84,23 @@ void World::handleSpecialKey(int key, int x, int y) {
 }
 
 void World::drawAxis() {
+
     glBegin(GL_LINES);
 
-	// X-axis in red
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(-100.0f, 0.0f, 0.0f);
-	glVertex3f(100.0f, 0.0f, 0.0f);
-	// Y Axis in Green
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, -100.0f, 0.0f);
-	glVertex3f(0.0f, 100.0f, 0.0f);
-	// Z Axis in Blue
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, -100.0f);
-	glVertex3f(0.0f, 0.0f, 100.0f);
+        /* X-axis in red */
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-100.0f, 0.0f, 0.0f);
+        glVertex3f(100.0f, 0.0f, 0.0f);
+
+        /* Y Axis in green */
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.0f, -100.0f, 0.0f);
+        glVertex3f(0.0f, 100.0f, 0.0f);
+        
+        /* Z Axis in blue */
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.0f, -100.0f);
+        glVertex3f(0.0f, 0.0f, 100.0f);
 
 	glEnd();
 }
