9de7c746185e932734b362fd5ea73c95aca6adc7
Rui Oliveira <rpoliveira02@gmail.com>
2023-03-08 21:11:35 +0000

    Quality of life improvements

--- include/parser.hpp
+++ include/parser.hpp
@@ -5,16 +5,16 @@
 
 #pragma once
 
+#include "model.hpp"
+#include "utils.hpp"
 #include <memory>
 #include <sstream>
 #include <string>
-#include "utils.hpp"
-#include "model.hpp"
 
 #ifdef FEDORA
-    #include "rapidxml.h"
+#include "rapidxml.h"
 #else
-    #include <rapidxml/rapidxml.hpp>
+#include <rapidxml/rapidxml.hpp>
 #endif
 
 #include "exceptions/invalid_xml_file.hpp"
@@ -26,124 +26,140 @@ typedef initializer_list<string> attrs;
 
 class XMLParser;
 
-template<typename... Ts> struct aux {
-    static tuple<Ts...> as_tuple(XMLParser& parser, attrs& attrs, attrs::iterator a);
-    static tuple<Ts...> as_tuple_opt(XMLParser& parser, attrs& attrs, attrs::iterator a);
+template <typename... Ts> struct aux {
+  static tuple<Ts...> as_tuple(XMLParser &parser, attrs &attrs,
+                               attrs::iterator a);
+  static tuple<Ts...> as_tuple_opt(XMLParser &parser, attrs &attrs,
+                                   attrs::iterator a);
 };
 
 /**
  * @brief A class representing a XML parser using rapidxml lib.
  */
 class XMLParser {
-    shared_ptr<string> content;
-    shared_ptr<xml_document<>> doc;
-    xml_node<> *node;
+  shared_ptr<string> content;
+  shared_ptr<xml_document<>> doc;
+  xml_node<> *node;
 
 public:
-    /**
-     * Default parser constructor.
-     * @param file_path Path of the XML file to be parsed.
-     */
-    explicit XMLParser(string file_path);
+  /**
+   * Default parser constructor.
+   * @param file_path Path of the XML file to be parsed.
+   */
+  explicit XMLParser(string file_path);
 
-    string name();
+  string name();
 
-    //throws exception if a child node is found with a name not in names
-    void validate_node(attrs names);
+  // throws exception if a child node is found with a name not in names
+  void validate_node(attrs names);
 
-    //throws exception if more than max child nodes with a name in names are found
-    void validate_max_nodes(int max, attrs names);
+  // throws exception if more than max child nodes with a name in names are
+  // found
+  void validate_max_nodes(int max, attrs names);
 
-    XMLParser get_node(string name);
-    vector<XMLParser> get_nodes();
-    vector<XMLParser> get_nodes(string name);
+  XMLParser get_node(string name);
+  vector<XMLParser> get_nodes();
+  vector<XMLParser> get_nodes(string name);
 
-    //throws exception if an attribute with a name not specified in attrs is found
-    void validate_attrs(attrs attrs);
+  // throws exception if an attribute with a name not specified in attrs is
+  // found
+  void validate_attrs(attrs attrs);
 
-    template<typename T> T get_attr(string name) {
-        for (xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute()) {
-            if (string(attr->name()) == name) {
-                stringstream convert(attr->value());
-                T value;
-                convert >> value;
-                return value;
-            }
-        }
-
-        throw InvalidXMLStructure("engine:get_attr : No such attribute exists in the node.");
+  template <typename T> T get_attr(string name) {
+    for (xml_attribute<> *attr = node->first_attribute(); attr;
+         attr = attr->next_attribute()) {
+      if (string(attr->name()) == name) {
+        stringstream convert(attr->value());
+        T value;
+        convert >> value;
+        return value;
+      }
     }
 
-    template<typename T> bool get_opt_attr(string name, T& ans) {
-        for (xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute()) {
-            if (string(attr->name()) == name) {
-                stringstream convert(attr->value());
-                convert >> ans;
-                return true;
-            }
-        }
+    throw InvalidXMLStructure(
+        "engine:get_attr : No such attribute exists in the node.");
+  }
 
-        return false;
-    }
-
-    template<typename... Ts> tuple<Ts...> as_tuple(attrs attrs) {
-        return aux<Ts...>::as_tuple(*this, attrs, attrs.begin());
-    }
-    
-    template<typename... Ts> tuple<Ts...> as_tuple_opt(attrs attrs) {
-        return aux<Ts...>::as_tuple_opt(*this, attrs, attrs.begin());
+  template <typename T> bool get_opt_attr(string name, T &ans) {
+    for (xml_attribute<> *attr = node->first_attribute(); attr;
+         attr = attr->next_attribute()) {
+      if (string(attr->name()) == name) {
+        stringstream convert(attr->value());
+        convert >> ans;
+        return true;
+      }
     }
 
-    template<typename T, typename... Args> T as_object(attrs attrs) {
-        return make_from_tuple<T>(as_tuple<Args...>(attrs));
-    }
+    return false;
+  }
 
-    template<typename T, typename... Args> T as_object_opt(attrs attrs) {
-        return make_from_tuple<T>(as_tuple_opt<Args...>(attrs));
-    }
+  template <typename... Ts> tuple<Ts...> as_tuple(attrs attrs) {
+    return aux<Ts...>::as_tuple(*this, attrs, attrs.begin());
+  }
+
+  template <typename... Ts> tuple<Ts...> as_tuple_opt(attrs attrs) {
+    return aux<Ts...>::as_tuple_opt(*this, attrs, attrs.begin());
+  }
+
+  template <typename T, typename... Args> T as_object(attrs attrs) {
+    return make_from_tuple<T>(as_tuple<Args...>(attrs));
+  }
+
+  template <typename T, typename... Args> T as_object_opt(attrs attrs) {
+    return make_from_tuple<T>(as_tuple_opt<Args...>(attrs));
+  }
 
 private:
-    XMLParser(shared_ptr<string> content, shared_ptr<xml_document<>> doc, xml_node<> *node);
+  XMLParser(shared_ptr<string> content, shared_ptr<xml_document<>> doc,
+            xml_node<> *node);
 };
 
-template<typename T, typename... Ts> struct aux<T, Ts...> {
-    static tuple<T,Ts...> as_tuple(XMLParser& parser, attrs& attrs, attrs::iterator a) {      
-        if (a == attrs.end())
-            throw InvalidXMLStructure("as_tuple: Too few arguments in initializer list.");
-        
-        T head = parser.get_attr<T>(*a);
-        tuple<Ts...> tail = aux<Ts...>::as_tuple(parser, attrs, a + 1);
+template <typename T, typename... Ts> struct aux<T, Ts...> {
+  static tuple<T, Ts...> as_tuple(XMLParser &parser, attrs &attrs,
+                                  attrs::iterator a) {
+    if (a == attrs.end())
+      throw InvalidXMLStructure(
+          "as_tuple: Too few arguments in initializer list.");
 
-        return tuple_cat(make_tuple(head), tail);
+    T head = parser.get_attr<T>(*a);
+    tuple<Ts...> tail = aux<Ts...>::as_tuple(parser, attrs, a + 1);
+
+    return tuple_cat(make_tuple(head), tail);
+  }
+
+  static tuple<T, Ts...> as_tuple_opt(XMLParser &parser, attrs &attrs,
+                                      attrs::iterator a) {
+    if (a == attrs.end())
+      throw InvalidXMLStructure(
+          "as_tuple_opt: Too few arguments in initializer list.");
+
+    T head;
+    tuple<Ts...> tail = aux<Ts...>::as_tuple_opt(parser, attrs, a + 1);
+
+    if (parser.get_opt_attr(*a, head)) {
+      return tuple_cat(make_tuple(head), tail);
+    } else {
+      return tail;
     }
-
-    static tuple<T,Ts...> as_tuple_opt(XMLParser& parser, attrs& attrs, attrs::iterator a) {      
-        if (a == attrs.end())
-            throw InvalidXMLStructure("as_tuple_opt: Too few arguments in initializer list.");
-        
-        T head;
-        tuple<Ts...> tail = aux<Ts...>::as_tuple_opt(parser, attrs, a + 1);
-
-        if (parser.get_opt_attr(*a, head)) {
-            return tuple_cat(make_tuple(head), tail);
-        } else {
-            return tail;
-        }
-    }
+  }
 };
 
-template<> struct aux<> {
-    static tuple<> as_tuple(__attribute__((unused)) XMLParser&, attrs& attrs, attrs::iterator a) {
-        if (a != attrs.end())
-            throw InvalidXMLStructure("as_tuple: Too many arguments in initializer list.");
-        
-        return { };
-    }
+template <> struct aux<> {
+  static tuple<> as_tuple(__attribute__((unused)) XMLParser &, attrs &attrs,
+                          attrs::iterator a) {
+    if (a != attrs.end())
+      throw InvalidXMLStructure(
+          "as_tuple: Too many arguments in initializer list.");
 
-    static tuple<> as_tuple_opt(__attribute__((unused)) XMLParser&, attrs& attrs, attrs::iterator a) {
-        if (a != attrs.end())
-            throw InvalidXMLStructure("as_tuple_opt: Too many arguments in initializer list.");
-        
-        return { };
-    }
+    return {};
+  }
+
+  static tuple<> as_tuple_opt(__attribute__((unused)) XMLParser &, attrs &attrs,
+                              attrs::iterator a) {
+    if (a != attrs.end())
+      throw InvalidXMLStructure(
+          "as_tuple_opt: Too many arguments in initializer list.");
+
+    return {};
+  }
 };
--- src/world.cpp
+++ src/world.cpp
@@ -6,60 +6,61 @@
 
 #include "utils.hpp"
 #include "world.hpp"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-World::World() {};
+World::World(){};
 
-World::World(WindowSize windowSize, Camera* camera, std::vector<Model> models) {
-    this->windowSize = windowSize;
-    this->camera = std::unique_ptr<Camera>(camera);
-    this->models = models;
+World::World(WindowSize windowSize, Camera *camera, std::vector<Model> models) {
+  this->windowSize = windowSize;
+  this->camera = std::unique_ptr<Camera>(camera);
+  this->models = models;
 }
 
 World::World(XMLParser parser) {
 
-    windowSize = parser.get_node("window").as_tuple<int,int>({"width", "height"});
-    camera = Camera::parse(parser.get_node({"camera"}));
-    models = std::vector<Model>();
+  windowSize =
+      parser.get_node("window").as_tuple<int, int>({"width", "height"});
+  camera = Camera::parse(parser.get_node({"camera"}));
+  models = std::vector<Model>();
 
-    XMLParser aux = parser.get_node("group").get_node("models");
+  XMLParser aux = parser.get_node("group").get_node("models");
 
-    for (XMLParser& p : aux.get_nodes("model")) {
+  for (XMLParser &p : aux.get_nodes("model")) {
 
-        string file_name = p.get_attr<string>("file");
+    string file_name = p.get_attr<string>("file");
 
-        std::ifstream file(file_name);
+    std::ifstream file(file_name);
 
-        if (!file) {
+    if (!file) {
 
-            std::stringstream exception_message;
-            exception_message << "XMLParser@model: The file '" << file_name << "' does not exits.";
-            throw InvalidXMLStructure(exception_message.str());
-        } 
-
-        models.emplace_back(p.as_object<Shape,string>({"file"}));
-
+      std::stringstream exception_message;
+      exception_message << "XMLParser@model: The file '" << file_name
+                        << "' does not exist.";
+      throw InvalidXMLStructure(exception_message.str());
     }
+    file.close();
+    models.emplace_back(p.as_object<Shape, string>({"file"}));
+  }
 }
 
 void World::initScene() {
 
-    // put GLUT's init here
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(std::get<0>(windowSize), std::get<1>(windowSize));
+  // put GLUT's init here
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowSize(std::get<0>(windowSize), std::get<1>(windowSize));
 
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Model Viewer 3000");
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Model Viewer 3000");
 
-    // some OpenGL settings
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  // some OpenGL settings
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 void World::changeSize(int width, int height) {
-    camera->changeSize({width, height});
+  camera->changeSize({width, height});
 }
 
 void World::renderScene() {
@@ -76,31 +77,34 @@ void World::renderScene() {
   glutSwapBuffers();
 }
 
-void World::handleKey(__attribute__((unused)) unsigned char key, __attribute__((unused)) int x, __attribute__((unused)) int y) {
-    camera->handleKey(key, x, y);
+void World::handleKey(__attribute__((unused)) unsigned char key,
+                      __attribute__((unused)) int x,
+                      __attribute__((unused)) int y) {
+  // Terminate the engine if 'q' is pressed
+  if (key == 'q')
+    exit(0); // TODO: More gracious shutdown
+  camera->handleKey(key, x, y);
 }
 
 void World::handleSpecialKey(int key, int x, int y) {
-    camera->handleSpecialKey(key, x, y);
+  camera->handleSpecialKey(key, x, y);
 }
 
 void World::drawAxis() {
-    glBegin(GL_LINES);
+  glBegin(GL_LINES);
 
-	// X-axis in red
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(-1000.0f, 0.0f, 0.0f);
-	glVertex3f(1000.0f, 0.0f, 0.0f);
-	// Y Axis in Green
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, -1000.0f, 0.0f);
-	glVertex3f(0.0f, 1000.0f, 0.0f);
-	// Z Axis in Blue
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, -1000.0f);
-	glVertex3f(0.0f, 0.0f, 1000.0f);
+  // X-axis in red
+  glColor3f(1.0f, 0.0f, 0.0f);
+  glVertex3f(-1000.0f, 0.0f, 0.0f);
+  glVertex3f(1000.0f, 0.0f, 0.0f);
+  // Y Axis in Green
+  glColor3f(0.0f, 1.0f, 0.0f);
+  glVertex3f(0.0f, -1000.0f, 0.0f);
+  glVertex3f(0.0f, 1000.0f, 0.0f);
+  // Z Axis in Blue
+  glColor3f(0.0f, 0.0f, 1.0f);
+  glVertex3f(0.0f, 0.0f, -1000.0f);
+  glVertex3f(0.0f, 0.0f, 1000.0f);
 
-	glEnd();
+  glEnd();
 }
-
-
