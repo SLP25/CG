6927c56ac3bf6a50468558ac76a52ac539b43727
tiago-bacelar <pereira.migueltiago@gmail.com>
2023-05-05 21:59:20 +0100

    Add comet orbit

--- src/scripts/ellipse_generator.py
+++ src/scripts/ellipse_generator.py
@@ -5,6 +5,43 @@ from collections import namedtuple
 
 Point = namedtuple("Point", "x y")
 
+def calculate_points_lame(dist: float, speed: float, attraction: float, amount: int):
+    delta = 0.01
+    result: list[Point] = []
+
+    x = dist
+    y = 0
+    vx = 0
+    vy = speed
+
+    halfway = False
+    
+    while not halfway or y < 0:
+        #calculate acceleration
+        d = np.sqrt(x ** 2 + y ** 2)
+        a = attraction / d ** 2
+        ax = - x * a / d
+        ay = - y * a / d
+
+        #update velocity
+        vx += ax * delta
+        vy += ay * delta
+
+        #update position
+        x += vx * delta
+        y += vy * delta
+
+        result.append(Point(x, y))
+
+        if y < 0:
+            halfway = True
+
+    result = result[::(len(result) // amount)]
+
+    for p in result:
+        print(f'<point x="{p.x}" y="0" z="{p.y}" />')
+
+    return result, [p.x for p in result], [p.y for p in result]
 
 def calculate_points_farther_density(width: float, height: float, amount: int = 100):
 
@@ -74,18 +111,14 @@ def calculate_points(width: float, height: float, amount: int) -> tuple[list[Poi
 
 if __name__ == "__main__":
 
-    points, xx, yy = calculate_points_farther_density(
-        width=10,
-        height=2.5,
-        amount=250
-    )
+    points, xx, yy = calculate_points_lame(30, 0.2, 7, 32)
 
     # Plot the points.
     plt.scatter(xx, yy, s=1)
 
     # Set the axis limits.
-    plt.xlim(-30, 30)
-    plt.ylim(-26, 26)
+    plt.xlim(-20, 40)
+    plt.ylim(-10, 10)
 
     # Set the aspect ratio to match the ellipse.
     plt.gca().set_aspect('equal', adjustable='box')
